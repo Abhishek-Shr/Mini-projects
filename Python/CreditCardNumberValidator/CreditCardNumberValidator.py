dc9110745ff61934fbe5be588455d1aae54ffab9
Anton Antonov <syndbe@gmail.com>
2014-02-17 01:30:09 +0200

    init

---
+++ Python/CreditCardNumberValidator/CreditCardNumberValidator.py
@@ -0,0 +1,37 @@
+
+inputCreditCardNumber = int(input("Credit card number: "))
+
+# check digit
+digit_16 = inputCreditCardNumber % 10
+# digits
+inputCreditCardNumber = int(inputCreditCardNumber / 10)
+digit_15 = inputCreditCardNumber % 10
+inputCreditCardNumber = int(inputCreditCardNumber / 10)
+digit_14 = inputCreditCardNumber % 10
+inputCreditCardNumber = int(inputCreditCardNumber / 10)
+digit_13 = inputCreditCardNumber % 10
+inputCreditCardNumber = int(inputCreditCardNumber / 10)
+digit_12 = inputCreditCardNumber % 10
+inputCreditCardNumber = int(inputCreditCardNumber / 10)
+digit_11 = inputCreditCardNumber % 10
+inputCreditCardNumber = int(inputCreditCardNumber / 10)
+digit_10 = inputCreditCardNumber % 10
+inputCreditCardNumber = int(inputCreditCardNumber / 10)
+digit_9 = inputCreditCardNumber % 10
+inputCreditCardNumber = int(inputCreditCardNumber / 10)
+digit_8 = inputCreditCardNumber % 10
+inputCreditCardNumber = int(inputCreditCardNumber / 10)
+digit_7 = inputCreditCardNumber % 10
+inputCreditCardNumber = int(inputCreditCardNumber / 10)
+digit_6 = inputCreditCardNumber % 10
+inputCreditCardNumber = int(inputCreditCardNumber / 10)
+digit_5 = inputCreditCardNumber % 10
+inputCreditCardNumber = int(inputCreditCardNumber / 10)
+digit_4 = inputCreditCardNumber % 10
+inputCreditCardNumber = int(inputCreditCardNumber / 10)
+digit_3 = inputCreditCardNumber % 10
+inputCreditCardNumber = int(inputCreditCardNumber / 10)
+digit_2 = inputCreditCardNumber % 10
+inputCreditCardNumber = int(inputCreditCardNumber / 10)
+digit_1 = inputCreditCardNumber % 10
+
